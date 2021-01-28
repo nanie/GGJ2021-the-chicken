b1017d9b32f2fcbefa1314d4bdf30d6b5076a54d
Camilla "Nanie" Camatta <camillacamatta@gmail.com>
2021-01-28 00:20:26 -0300

    Update Meuscriptdetestemaluco.cs

--- Assets/Scripts/Meuscriptdetestemaluco.cs
+++ Assets/Scripts/Meuscriptdetestemaluco.cs
@@ -7,7 +7,8 @@ public class Meuscriptdetestemaluco : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        new GameObject();
+        //Comentario inutil
     }
 
     // Update is called once per frame
