6ae2a55272242df7d3a7d4ed3b34e8c83625ad2f
GuusP <gustavo.vinicius.paulino@gmail.com>
2021-01-28 17:26:45 -0300

    Follower Enemy 1

---
+++ Assets/Scripts/Enemies/FollowerEnemy.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class FollowerEnemy : BaseEnemy
+{
+    Transform target;
+    bool following = false;
+    [SerializeField]
+    private float attackDistance = 0.2f;
+    [SerializeField]
+    private float attackTime = 1f;
+  
+    
+    private float timerAttack;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+      timerAttack = attackTime;
+      
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(following == true && Vector2.Distance(target.position, transform.position) <= attackDistance)
+        {
+            timerAttack -= Time.deltaTime;
+            if (timerAttack <= 0)
+            {
+                timerAttack = attackTime;
+                SetDamage(target.gameObject);
+            }
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            StartFollow(collision.transform);
+        }
+    }
+
+    void StartFollow(Transform target)
+    {
+        this.target = target;
+        following = true;
+        AIPath aI = GetComponent<AIPath>(); 
+        AIDestinationSetter aIDestination = GetComponent<AIDestinationSetter>();
+        aIDestination.target = target;
+
+        
+    }
+}
