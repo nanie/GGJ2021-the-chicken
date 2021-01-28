3e0c2244f026234343542da2fbb406ce6927ff2d
Camilla "Nanie" Camatta <camillacamatta@gmail.com>
2021-01-28 11:44:17 -0300

    Controle adicionado

---
+++ Assets/Scripts/IAnimatorManager.cs
@@ -0,0 +1,8 @@
+﻿
+using UnityEngine;
+
+public interface IAnimatorManager 
+{
+    void SetWalking(bool IsWalking);
+    void SetDirection(Vector2 direction);
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    //Player movement speed
+    private float speed = 5f;
+
+    //Character Rigidy Body
+    private Rigidbody2D rigidBody;
+
+    //Horizontal and vertical axis from player input
+    [HideInInspector]
+    public Vector2 direction;
+    [Header("Common settings")]
+    [SerializeField] private string horizontalAxis = "Horizontal";
+    [SerializeField] private string verticalAxis = "Vertical";
+    private IAnimatorManager animator;
+    [Header("Flip Renderer settings")]
+    [SerializeField] private bool flipHorizontal;
+    [SerializeField] private HorizontalDirection originalSpriteDirection;
+
+    private SpriteRenderer[] spriteRenderers;
+    private bool _movementStarted = true;
+    void Start()
+    {
+        animator = GetComponentInChildren<IAnimatorManager>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        //Find input axis
+        direction.x = Input.GetAxis(horizontalAxis);
+        direction.y = Input.GetAxis(verticalAxis);
+        //Flip sprites if needed
+        if (flipHorizontal)
+        {
+            foreach (var item in spriteRenderers)
+            {
+                item.flipX = originalSpriteDirection == HorizontalDirection.left? direction.x > 0 : direction.x < 0;
+                
+            }
+        }
+
+        if (animator != null && direction.sqrMagnitude > 0)
+        {
+            _movementStarted = false;
+            animator.SetDirection(direction);
+        }
+        else if (!_movementStarted)
+        {
+            animator.SetWalking(false);
+            _movementStarted = true;
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        rigidBody.MovePosition(rigidBody.position + direction * speed * Time.fixedDeltaTime);
+    }
+
+}
